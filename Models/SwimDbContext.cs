b84eb45007c153a1550b2caac22b2f6bcb5ea3e1
Scxe <ajthompson2@cougars.ccis.edu>
2020-10-10 19:58:31 -0500

    Added Models, AdminController, and Index.cshtml. Not all files are complete.

---
+++ Models/SwimDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CISS411_Project.Models
+{
+    public class SwimDbContext : DbContext
+    {
+
+    }
+}
