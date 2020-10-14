af00df9196c79e6265df88f7e449aecf89b56bb4
shaunhembree <shaunhembree@yahoo.com>
2020-10-14 14:38:05 -0500

    Changes made Shaun
    I added a few things from chapter 7, the only thing that is not working correctly is the Register on the AdminController, not sure what i did wrong there.

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -2,15 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CISS411_Project.Models;
+using CISS411_Project.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CISS411_Project.Controllers
 {
     public class AdminController : Controller
     {
-        public IActionResult Index()
+        private SwimDbContext db;
+        private UserManager<RegisteredUser> userManager;
+        private SignInManager<RegisteredUser> signInManager;
+        private RoleManager<IdentityRole> roleManager;
+        public AdminController(UserManager<RegisteredUser> userManager,
+            SignInManager<RegisteredUser> signInManager,
+            RoleManager<IdentityRole> roleManager, SwimDbContext db)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.roleManager = roleManager;
+            this.db = db;
+        }
+        public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(AdminRegisterViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new RegisteredUser { UserName = vm.Email, Email = vm.Email };
+                var result = await userManager.CreateAsync(user, vm.Password);
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+
+            }
         }
     }
 }
